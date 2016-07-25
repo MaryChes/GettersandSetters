732a64a2e7627874446c3d380288a209e22e874e
MaryChes <chessie4907@gmail.com>
2016-07-25 16:42:36 -0400

    first commit 7/25/2016

---
+++ SettersAndGetters.java
@@ -0,0 +1,161 @@
+package com.ssa.datamembers;
+
+import java.util.Date;
+
+public class SettersAndGetters 
+{
+private float sizeOfDisk;
+
+public float getSizeOfDisk() 
+{
+	return sizeOfDisk;
+}
+
+public void setSizeOfDisk(float sizeOfDisk) 
+{
+	this.sizeOfDisk = sizeOfDisk;
+}
+
+
+private int numberOfAcresLand;
+
+public int getNumberOfAcresLand() 
+{
+	return numberOfAcresLand;
+}
+
+public void setNumberOfAcresLand(int numberOfAcresLand) 
+{
+	this.numberOfAcresLand = numberOfAcresLand;
+}
+
+
+private String fileName;
+
+public String getFileName() 
+{
+	return fileName;
+}
+
+public void setFileName(String fileName) 
+{
+	this.fileName = fileName;
+}
+
+
+private String nineDigitPhoneNumber;
+
+public String getNineDigitPhoneNumber()
+{
+	return nineDigitPhoneNumber;
+}
+
+public void setNineDigitPhoneNumber(String nineDigitPhoneNumber) 
+{
+	this.nineDigitPhoneNumber = nineDigitPhoneNumber;
+}
+
+
+private String companyName;
+
+public String getCompanyName() 
+{
+	return companyName;
+}
+
+public void setCompanyName(String companyName) 
+{
+	this.companyName = companyName;
+}
+
+
+private Date todaysDate;
+
+public Date getTodaysDate() 
+{
+	return todaysDate;
+}
+
+public void setTodaysDate(Date todaysDate) 
+{
+	this.todaysDate = todaysDate;
+}
+
+
+private boolean stateOfLightSwitch;
+
+public boolean isStateOfLightSwitch() 
+{
+	return stateOfLightSwitch;
+}
+
+public void setStateOfLightSwitch(boolean stateOfLightSwitch) 
+{
+	this.stateOfLightSwitch = stateOfLightSwitch;
+}
+
+
+private double pi;
+
+public double getPi() 
+{
+	return pi;
+}
+
+public void setPi(double pi) 
+{
+	this.pi = pi;
+}
+
+
+private long speedOfLightInAVacuum;
+
+public long getSpeedOfLightInAVacuum() 
+{
+	return speedOfLightInAVacuum;
+}
+
+public void setSpeedOfLightInAVacuum(long speedOfLightInAVacuum) 
+{
+	this.speedOfLightInAVacuum = speedOfLightInAVacuum;
+}
+
+
+private Vehicle v;
+
+public Vehicle getV() 
+{
+	return v;
+}
+
+public void setV(Vehicle v) 
+{
+	this.v = v;
+}
+
+
+private Address a;
+
+public Address getA() 
+{
+	return a;
+}
+
+public void setA(Address a) 
+{
+	this.a = a;
+}
+
+private Product[] p;
+
+public Product[] getP() 
+{
+	return p;
+}
+
+public void setP(Product[] p) 
+{
+	this.p = p;
+}
+
+}
